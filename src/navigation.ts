dd8b950841398faf7a9af5e3b64654fe4051bf5b
microlab-dev <59581197+microlab-dev@users.noreply.github.com>
2024-12-23 17:16:49 +0100

    added some brands, other

--- src/navigation.ts
+++ src/navigation.ts
@@ -82,7 +82,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ], */
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="NordicAlpha logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://microlab.dev/static/media/logo.b3dda4f4.svg" alt="NordicAlpha logo" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://microlab.dev/"> microlab</a> · All rights reserved.
   `,
 };
